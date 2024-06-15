d68cc39ba6efb3f35c885acf1c2a074e90c08cfa
TofikE124 <eleltofik@gmail.com>
2024-06-15 16:01:29 +0300

    Adding filter panel, modifying panel

--- src/app/components/home/search-section/search-section-small/search-section-small.component.ts
+++ src/app/components/home/search-section/search-section-small/search-section-small.component.ts
@@ -1,3 +1,4 @@
+import { PanelService } from './../../../../services/panel.service';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -6,6 +7,8 @@ import { ThemeService } from '../../../../services/theme.service';
 import { SearchBarComponent } from '../../../form/search-bar/search-bar.component';
 import { IconButtonComponent } from '../../../icon-button/icon-button.component';
 import { IconComponent } from '../../../icon/icon.component';
+import { PanelComponent } from '../../../panel/panel.component';
+import { CheckboxComponent } from '../../../form/checkbox/checkbox.component';
 
 @Component({
   selector: 'search-section-small',
@@ -15,16 +18,21 @@ import { IconComponent } from '../../../icon/icon.component';
     CommonModule,
     IconButtonComponent,
     IconComponent,
+    PanelComponent,
+    CheckboxComponent,
   ],
   templateUrl: './search-section-small.component.html',
   styleUrl: './search-section-small.component.scss',
 })
 export class SearchSectionSmallComponent {
   isDark$: Observable<boolean>;
-  constructor(themeService: ThemeService) {
+  constructor(themeService: ThemeService, public panelService: PanelService) {
     this.isDark$ = themeService.isDark$;
   }
 
   searchIconType = IconType.SEARCH;
   filterIconType = IconType.FILTER;
+  locationIconType = IconType.LOCATION;
+
+  panelName = 'small-search-section-panel';
 }
